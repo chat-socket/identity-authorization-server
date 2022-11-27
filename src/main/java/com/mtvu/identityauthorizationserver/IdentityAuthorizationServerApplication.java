a4b47527ff5d3b57f89acec80eb38b9dc0588ee1
Manh Tu VU <glmanhtu@gmail.com>
2022-11-27 22:09:05 +0100

    first commit

---
+++ src/main/java/com/mtvu/identityauthorizationserver/IdentityAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.mtvu.identityauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAuthorizationServerApplication.class, args);
+	}
+
+}
