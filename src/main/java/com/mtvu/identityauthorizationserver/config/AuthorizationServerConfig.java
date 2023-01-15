a4ac2aa5202330739cc6d14223b3b999f2572017
VU Manh Tu <glmanhtu@gmail.com>
2023-01-15 10:56:31 +0700

    Set the duration of access Token

--- src/main/java/com/mtvu/identityauthorizationserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/mtvu/identityauthorizationserver/config/AuthorizationServerConfig.java
@@ -52,6 +52,7 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -60,6 +61,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -145,6 +147,10 @@ public class AuthorizationServerConfig {
 					.authorizationGrantTypes(x -> x.addAll(grantTypes))
 					.redirectUris(x -> x.addAll(properties.getRedirectUris()))
 					.scopes((x) -> x.addAll(properties.getScopes()))
+					.tokenSettings(TokenSettings.builder()
+							.accessTokenTimeToLive(Duration.ofMinutes(5))
+							.refreshTokenTimeToLive(Duration.ofHours(2))
+							.build())
 					.clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
 					.build();
 			registeredClientRepository.save(registeredClient);
